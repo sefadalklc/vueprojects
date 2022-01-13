9ed0c44bd19d85c4edefa9a1612738bdd6e422a1
sefadalklc <sefadalk11@gmail.com>
2022-01-14 00:21:56 +0300

    projeye başlarken

---
+++ blogapp/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="nav">
+    
+  <router-view/>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Yusei+Magic&display=swap');
+
+
+body{
+  max-width: 1200px;
+  margin: 20px;
+  padding: 0 20px;
+  font-family: 'Yusei Magic', sans-serif;
+  background-color: #dfe4ea;
+
+}
+
+p,h1,h2,h3,a,ul{
+  margin: 0px;
+  padding: 0px;
+  text-decoration: none;
+  color: #333;
+}
+h2{
+  margin-bottom: 40px;
+}
+h3{
+  text-transform:capitalize ;
+  margin-bottom: 8px;
+}
+</style>
