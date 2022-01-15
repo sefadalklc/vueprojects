cdc4fbf4059811b44b2275d88c8179c2fa3cd98f
sefadalklc <sefadalk11@gmail.com>
2022-01-15 23:52:23 +0300

    home sayfası

--- blogapp/src/views/Home.vue
+++ blogapp/src/views/Home.vue
@@ -1,14 +1,37 @@
 <template>
-  
+  <div class="home">
+      <div v-if="hatalar">
+          {{hatalar}}
+      </div>
+      <div v-else-if="makaleler.length">
+          <MakaleListesi :makaleler="makaleler" />
+      </div>
+      <div v-else>
+          Yükleniyor ... 
+      </div>
+  </div>
 </template>
 
 <script>
-export default {
 
+import MakaleListesi from '../components/MakaleListesi.vue';
+import makalelerGetir from '../composables/makalelerGetir';
+
+export default {
+    name:'Home',
+    components:{
+        MakaleListesi
+    },
+    setup(){
+        const{makaleler,hatalar,makaleListesiniYukle}=makalelerGetir();
+        makaleListesiniYukle();
+
+        return {makaleler,hatalar}
+    }
 }
 </script>
 
 <style>
 
 </style>
-</script>
+
